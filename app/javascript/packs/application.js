cdaec4ffc8bd3277a3bda9ce62c6b0c964e6b2bf
brunocamps <bruno.mcamps@outlook.com>
2022-10-31 06:42:44 -0300

    Messages partial

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,4 +14,5 @@ ActiveStorage.start()
 
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
-require("stylesheets/application.scss")     
+require("stylesheets/application.scss")  
+   
